51be98db3cd466ed2a3c120fce761196ff6692b7
KAAN BAL <balkaan01@gmail.com>
2022-04-27 21:19:07 +0300

    SQL PATIKA.DEV CEVAPLARİ9

---
+++ calisma9.sql
@@ -0,0 +1,13 @@
+----------------- ODEV 9 CEVAPLARI
+
+--------- CEVAP 1
+-- SELECT city, country FROM city
+-- JOIN country ON city.country_id = country.country_id;
+
+--------- CEVAP 2
+-- SELECT payment_id, first_name, last_name FROM customer
+-- JOIN payment ON payment.customer_id = customer.customer_id;
+
+--------- CEVAP 3
+-- SELECT rental_id, first_name, last_name FROM customer
+-- JOIN rental ON rental.customer_id = customer.customer_id;
