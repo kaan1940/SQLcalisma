49b9ee704f43e73ae9891c4b813c39c07606323f
KAAN BAL <balkaan01@gmail.com>
2022-04-28 21:31:39 +0300

    SQL PATIKA.DEV CEVAPLARİ11

---
+++ calisma11.sql
@@ -0,0 +1,28 @@
+--------------------- ODEV 11 CEVAPLARI
+
+---------- CEVAP 1
+-- (
+-- SELECT first_name FROM actor
+-- )
+-- UNION ALL
+-- (
+-- SELECT first_name FROM customer
+-- );
+
+--------- CEVAP 2
+-- (
+-- SELECT first_name FROM actor
+-- )
+-- INTERSECT
+-- (
+-- SELECT first_name FROM customer
+--);
+
+--------- CEVAP 3
+-- (
+-- SELECT first_name FROM actor
+-- )
+-- EXCEPT
+-- (
+-- SELECT first_name FROM customer
+-- );
